e446417592f2b61a44dc71164390e97c7b8bf4a2
Kanan302 <kananalizade4@gmail.com>
2025-04-20 18:31:49 +0400

    added scan function

--- lib/ui/views/home/profile_tab/pages/do_scan/do_scan.dart
+++ lib/ui/views/home/profile_tab/pages/do_scan/do_scan.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+import '../../../../../../shared/theme/app_colors.dart';
 
 class DoScanPage extends StatefulWidget {
   const DoScanPage({super.key});
@@ -8,11 +13,83 @@ class DoScanPage extends StatefulWidget {
 }
 
 class _DoScanPageState extends State<DoScanPage> {
+  final MobileScannerController controller = MobileScannerController();
+  StreamSubscription<BarcodeCapture>? subscription;
+  String? scannedCode;
+  bool isScanned = false;
+
+  void listenToBarcodes() {
+    subscription = controller.barcodes.listen((BarcodeCapture capture) {
+      final barcode = capture.barcodes.first;
+      final code = barcode.rawValue;
+
+      if (code != null && !isScanned) {
+        setState(() {
+          scannedCode = code;
+          isScanned = true;
+        });
+        debugPrint('scanned code is: $scannedCode');
+        Future.delayed(const Duration(seconds: 2), () {
+          setState(() => isScanned = false);
+        });
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    listenToBarcodes();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Do Scan')),
-      body: SafeArea(child: Center(child: Text('do scan page'))),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: Icon(Icons.arrow_back_ios_new_outlined),
+        ),
+      ),
+      body: Stack(
+        children: [
+          MobileScanner(controller: controller, fit: BoxFit.cover),
+          Center(
+            child: Container(
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                border: Border.all(color: AppColors.white, width: 4),
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+          ),
+
+          if (scannedCode != null)
+            Positioned(
+              left: 10,
+              bottom: 10,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.black.withValues(alpha: 0.6 * 255),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                padding: const EdgeInsets.all(12),
+                child: Text(
+                  'Code: $scannedCode',
+                  style: const TextStyle(color: AppColors.white),
+                ),
+              ),
+            ),
+        ],
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    subscription?.cancel();
+    controller.dispose();
+    super.dispose();
   }
 }
