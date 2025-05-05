17afbd4d2a45aa3e7a382ccacce45ce114f5ae00
Kanan302 <kananalizade4@gmail.com>
2025-05-05 11:03:16 +0400

    l10 folder moved into shared folder

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -4,7 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-import '../l10n/l10n.dart';
+import '../shared/l10n/l10n.dart';
 import '../shared/services/navigation/app_router.dart';
 import '../ui/views/home/profile_tab/service/locale_cubit.dart';
 import '../ui/views/home/profile_tab/service/theme_cubit.dart';
