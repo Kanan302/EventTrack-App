15c7ea61d296bc748b985c6319c877c82e9d9797
Kanan302 <kananalizade4@gmail.com>
2025-05-17 00:58:11 +0400

    added error message for register event button

--- lib/data/repositories/events/register_event/register_event_repository.dart
+++ lib/data/repositories/events/register_event/register_event_repository.dart
@@ -20,9 +20,13 @@ class RegisterEventRepository {
     } on DioException catch (e) {
       final statusCode = e.response?.statusCode;
       final errorMessage =
-          e.message ?? AppLocalizations.of(context).unknownError;
+          e.response?.data['message'] ??
+          AppLocalizations.of(context).unknownError;
+
       if (statusCode == 404) {
         throw Exception(AppLocalizations.of(context).notFoundEvents);
+      } else if (statusCode == 400) {
+        throw Exception(AppLocalizations.of(context).userAlreadyRegistered);
       } else if (statusCode == 500) {
         throw Exception(AppLocalizations.of(context).problemWithSystem);
       } else {
